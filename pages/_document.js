0c7de5abcab7e4e5c216aa3ad746cc20e3e79f79
maggie-j-liu <maggie.j.liu@gmail.com>
2021-10-10 10:33:12 -0700

    uwu favicon

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/uwu.svg" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
